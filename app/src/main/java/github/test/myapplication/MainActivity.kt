7cfc1ae824ef7da5dc91f10301cae2c4a2ec6ec2
alireza <alireza.pahlavanzadeh71@gmail.com>
2018-09-01 19:55:59 +0430

    testing

--- app/src/main/java/github/test/myapplication/MainActivity.kt
+++ app/src/main/java/github/test/myapplication/MainActivity.kt
@@ -1,5 +1,4 @@
 package github.test.myapplication
-
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,13 +6,10 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val task = getDataFromNet(object : TaskListner {
             override fun onResult(result: String) {
                 Toast.makeText(applicationContext,result,Toast.LENGTH_SHORT).show()
@@ -22,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         var test=findViewById(R.id.test) as TextView
         test.setOnClickListener(View.OnClickListener {
             Toast.makeText(applicationContext,"clicked",Toast.LENGTH_SHORT).show()
-
         })
     }
     interface TaskListner
@@ -34,14 +29,10 @@ class MainActivity : AppCompatActivity() {
         override fun doInBackground(vararg params: Void?): Any {
             return "salam"
         }
-
-
         var x:TaskListner
         init {
            x=task
         }
-
-
         override fun onPostExecute(result: Any?) {
             super.onPostExecute(result)
             x.onResult(result.toString())
